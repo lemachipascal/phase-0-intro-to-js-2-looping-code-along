2912b4efcdbb292aaa20eab7a987cf421ee8f2b8
Lemachipascal-dev <lemachijo421@gmail.com>
2024-07-22 11:59:43 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, graduation){
+    let messages = [];
+    
+    for (let i=0; i< names.length; i++){
+        
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+
+        messages.push(message);
+    }
+
+    return messages;
+
+}
+
+writeCards(['Guadalupe', 'Ollie', 'Aki'], 'graduation');
+
+//while loop
+function countDown(integer){
+    while(integer >= 0 ){
+        console.log(integer)
+        integer --
+    }
+}
+countDown();
